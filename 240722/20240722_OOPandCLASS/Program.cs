d1214637f94fb34c6bc7338ce1e628474f218366
koigyokou <cksthfakdhkd@gmail.com>
2024-07-22 15:32:53 +0900

    add 20240722_OOPandCLASS

---
+++ 240722/20240722_OOPandCLASS/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace _20240722_OOPandCLASS
+{
+    class Character
+    {
+        int level;
+        int hp;
+        float speed;
+        int attackPower;
+
+        void MoveForward()
+        {
+
+        }
+        void MoveBackward()
+        {
+
+        }
+        void TurnLeft90degree()
+        {
+
+        }
+        void TurnRight90degree()
+        {
+
+        }
+        void Attack()
+        {
+
+        }
+        void BeAttacked()
+        {
+
+        }
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
